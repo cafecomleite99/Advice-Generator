0a51537168421044c19dfdf0a7bc890b0c0505a4
Leonardo Sousa <leonardoiself2@gmail.com>
2022-03-08 17:03:15 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const adviceId = document.querySelector('#adviceID')
+const adviceContent = document.querySelector('#adviceContent')
+const randomBtn = document.querySelector('#randomBtn')
+
+async function setRandomAdvice() {
+    let res = await fetch('https://api.adviceslip.com/advice').then((resp) => resp.json())
+
+    adviceId.innerHTML = `ADVICE #${res.slip.id}`
+    adviceContent.innerHTML = res.slip.advice
+}
+
+randomBtn.addEventListener('click', setRandomAdvice)
